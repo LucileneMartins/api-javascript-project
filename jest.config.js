615767aac18c77939d28d3f819ea16a647656636
LucileneMartins <45217274+LucileneMartins@users.noreply.github.com>
2021-03-31 20:11:03 -0300

    mudanças config.yml e configuração jest config json

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,18 @@ module.exports = {
     testTimeout: 20000,
     transform: {
         "^.+\\.jsx?$": "babel-jest"
-    }
-    
+    },
+    "reporters": [
+      "default",
+      ["jest-html-reporters", {
+        "publicPath": "./report",
+        "filename": "report.html",
+        "pageTitle":"Report Api-Test",
+        "expand": true,
+        "openReport": true
+      }]
+
+    ]
   };
+
+  
